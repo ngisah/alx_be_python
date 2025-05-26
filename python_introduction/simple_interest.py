dcd62982f092d6acdff64794db64758949df845b
ngisah <ngisahedwin@gmail.com>
2025-05-26 12:02:09 +0300

    calculation correction

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,5 @@
 principal = 1000  # Principal amount in dollars
 rate = 0.05  # Interest rate in decimal
 time = 3  # Time in years
-interest = (principal * rate * time)
+interest = principal * rate * time
 print(f"The simple interest is: {interest}")
